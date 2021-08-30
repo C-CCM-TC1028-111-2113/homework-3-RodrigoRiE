434c9a77e90b64574e9e6ce912378cef81798339
RodrigoRiE <88850926+RodrigoRiE@users.noreply.github.com>
2021-08-29 23:07:51 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,16 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    año = int(input("Inserte un año: ")) 
+
+    if año % 4 != 0: 
+        print("No es bisiesto")
+    elif año % 4 == 0 and año % 100 != 0: 
+        print("Es bisiesto")
+    elif año % 4 == 0 and año % 100 == 0 and año % 400 != 0:
+        print("No es bisiesto")
+    elif año % 4 == 0 and año % 100 == 0 and año % 400 == 0: 
+        print("Es bisiesto")
     pass
 
 if __name__=='__main__':
